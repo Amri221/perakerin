7212a1fb126732430ba502c2ad02c18fea742989
Amri221 <shinkay177@gmail.com>
2024-07-21 22:16:56 +0700

    21 Juli 2024

---
+++ Jawa.java
@@ -0,0 +1,65 @@
+public class Jawa {
+    public static void main(String[] args) {
+    //Percobaan pertama cihuy
+        System.out.println("Bismillah");
+        System.out.println(3*5);
+
+    //Percobaan kedua cihuy
+   /*  String name = "meri cihuy kok 5 aku bingung harusnya sekarang 17 \n ini new line sini lu gw timbuk cihuyy \t coba tab";
+    System.out.println(name);
+    int mynum = 2; //number
+    mynum = 4;
+    System.out.println(mynum);
+    float myFloatnum = 5.99f; //Floating point number
+    char myletter = 'C'; //Character
+    boolean myBool = true ; //Boolean
+    String myText = "Halah mending bobok "; //String
+    System.out.println(myFloatnum);
+    System.out.println(myletter);
+    System.out.println(myBool);
+    System.out.println(myText);
+    */
+
+    //Percobaan ketiga
+    int z = 12;  
+    int s = 12;
+    String name = "Halo nama saya amari \n dan sekarang waktunya java cihuyy";
+    System.out.println(z + s);
+    byte number12 = 120;
+    System.out.println(number12);
+
+    float f1 = 12e34f;
+    System.out.println(f1);
+
+    int nomor = 10;
+    double sheshh = nomor;
+    System.out.println(nomor);
+    System.out.println(sheshh);
+
+    System.out.println("The length of the string is : " + name.length());
+    System.out.println(name.indexOf("sekarang"));
+
+    String last = "putra";
+    System.out.println(name + " " + last);
+
+    String mani = "We are the so-called \"Vikings\" from the north"; // Gunakan \"\" untuk tanda kutip
+    System.out.println(mani);
+
+    System.out.println(Math.sqrt(64)); //
+    System.out.println(Math.min(9, 10)); // Membandingakn dua angka mana yang lebih kecil
+    System.out.println(Math.max(89, 19)); // Membandingkan dua angka mana yang lebih besar
+    System.out.println(Math.abs(-3.2)); // Membuat angka menjadi bilangan positif
+    int randomNumb = (int)(Math.random() * 101); //angka random dari jangka 0 sampai 100
+    System.out.println(randomNumb);
+    int randomNumb2 =(int)(Math.random()*99);
+
+    System.out.println(nomor > number12);
+    System.out.println(number12 > nomor);
+
+    if (randomNumb < randomNumb2){
+        System.out.println("Aduhhhh");
+    }else{
+        System.out.println("mantap wehhh");
+    }
+    }
+}
